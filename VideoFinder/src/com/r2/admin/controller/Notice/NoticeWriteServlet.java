a4b0bf3703aa973529f0db2a910e335fbcca82fd
gihyeon <gihyeon6394@gmail.com>
2019-08-10 12:58:30 +0900

    김기현 관리자 git 재테스트

---
+++ VideoFinder/src/com/r2/admin/controller/Notice/NoticeWriteServlet.java
@@ -0,0 +1,60 @@
+package com.r2.admin.controller.Notice;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.r2.admin.model.service.NoticeService;
+import com.r2.admin.model.vo.Notice;
+
+/**
+ * Servlet implementation class NoticeWriteServlet
+ */
+@WebServlet("/admin/writeNotice")
+public class NoticeWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeWriteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ㅋㅋㅋㅋ");
+		String notice_Category = request.getParameter("notice_Category");
+		String notice_Title = request.getParameter("notice_Title");
+		String notice_Content = request.getParameter("notice_Content");
+		
+		
+		Notice n = new Notice();
+		n.setNotice_Category(notice_Category);
+		n.setNotice_Title(notice_Title);
+		n.setNotice_Content(notice_Content);
+		
+		int result = new NoticeService().insertNotice(n);
+		
+		if(result > 0) {
+			
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
